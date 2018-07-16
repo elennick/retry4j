acc32d18d6a00c2b2b0b846f5134cfcf5c676a94
Evan Lennick <evan.lennick@accolade.com>
2018-07-16 14:29:03 -0400

    added more tests

--- src/test/java/com/evanlennick/retry4j/CallExecutorTest_RetryOnCustomLogicTest.java
+++ src/test/java/com/evanlennick/retry4j/CallExecutorTest_RetryOnCustomLogicTest.java
@@ -4,7 +4,6 @@ import com.evanlennick.retry4j.config.RetryConfig;
 import com.evanlennick.retry4j.config.RetryConfigBuilder;
 import com.evanlennick.retry4j.exception.RetriesExhaustedException;
 import com.evanlennick.retry4j.exception.UnexpectedException;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.time.Duration;
@@ -14,20 +13,15 @@ import static org.testng.Assert.fail;
 
 public class CallExecutorTest_RetryOnCustomLogicTest {
 
-    private static RetryConfig config;
-
-    @BeforeClass
-    public static void setup() {
-        config = new RetryConfigBuilder()
+    @Test
+    public void verifyShouldRetryOnExceptionMessage() {
+        RetryConfig config = new RetryConfigBuilder()
                 .retryOnCustomExceptionLogic(ex -> ex.getMessage().contains("should retry!"))
                 .withFixedBackoff()
                 .withDelayBetweenTries(Duration.ofMillis(1))
                 .withMaxNumberOfTries(3)
                 .build();
-    }
 
-    @Test
-    public void verifyShouldRetry() {
         try {
             new CallExecutor<>(config)
                     .execute(() -> {
@@ -40,10 +34,66 @@ public class CallExecutorTest_RetryOnCustomLogicTest {
     }
 
     @Test(expected = UnexpectedException.class)
-    public void verifyShouldNotRetry() {
+    public void verifyShouldNotRetryOnExceptionMessage() {
+        RetryConfig config = new RetryConfigBuilder()
+                .retryOnCustomExceptionLogic(ex -> ex.getMessage().contains("should retry!"))
+                .withFixedBackoff()
+                .withDelayBetweenTries(Duration.ofMillis(1))
+                .withMaxNumberOfTries(3)
+                .build();
+
         new CallExecutor<>(config)
                 .execute(() -> {
                     throw new RuntimeException("should NOT retry!");
                 });
+    }
+
+    @Test
+    public void verifyShouldRetryOnCustomException() {
+        RetryConfig config = new RetryConfigBuilder()
+                .retryOnCustomExceptionLogic(ex -> ((CustomTestException) ex).getSomeValue() > 0)
+                .withFixedBackoff()
+                .withDelayBetweenTries(Duration.ofMillis(1))
+                .withMaxNumberOfTries(3)
+                .build();
+
+        try {
+            new CallExecutor<>(config)
+                    .execute(() -> {
+                        throw new CustomTestException("should retry!", 100);
+                    });
+            fail();
+        } catch (RetriesExhaustedException e) {
+            assertThat(e.getStatus().getTotalTries()).isEqualTo(3);
+        }
+    }
+
+    @Test(expected = UnexpectedException.class)
+    public void verifyShouldNotRetryOnCustomException() {
+        RetryConfig config = new RetryConfigBuilder()
+                .retryOnCustomExceptionLogic(ex -> ((CustomTestException) ex).getSomeValue() > 0)
+                .withFixedBackoff()
+                .withDelayBetweenTries(Duration.ofMillis(1))
+                .withMaxNumberOfTries(3)
+                .build();
+
+        new CallExecutor<>(config)
+                .execute(() -> {
+                    throw new CustomTestException("test message", -100);
+                });
+    }
+
+    private class CustomTestException extends RuntimeException {
+
+        private int someValue;
+
+        public CustomTestException(String message, int someValue) {
+            super(message);
+            this.someValue = someValue;
+        }
+
+        public int getSomeValue() {
+            return someValue;
+        }
     }
 }
